36d2219091ec7f9da7c8c1e5f34a8c4cd8bc7296
55555 <glinskaya33@mail.ru>
2022-10-29 00:40:12 +0300

    HW12

--- src/BooK.java
+++ src/BooK.java
@@ -1,2 +1,35 @@
 public class BooK {
+    private String titleOfTheBook;
+    private int yaerOfPublicationOfTheBook;
+    private Author fullName;
+    public BooK(String titleOfTheBook,int yaerOfPublicationOfTheBook,Author fullName) {
+        this.titleOfTheBook = titleOfTheBook;
+        this.yaerOfPublicationOfTheBook = yaerOfPublicationOfTheBook;
+        this.fullName = fullName;
+    }
+    public Author getFullName(){
+        return this.fullName;
+    }
+    public String getTitleOfTheBook() {
+        return this.titleOfTheBook;
+    }
+    public int getYaerOfPublicationOfTheBook() {
+        return this.yaerOfPublicationOfTheBook;
+    }
+    public void setYaerOfPublicationOfTheBook(int yaerOfPublicationOfTheBook) {
+        this.yaerOfPublicationOfTheBook = yaerOfPublicationOfTheBook;
+    }
+    @Override
+    public String toString() {
+        return "BooK{" +
+                "titleOfTheBook='" + titleOfTheBook + '\'' +
+                ", yearOfThePublicationOfTheBook=" + yaerOfPublicationOfTheBook +
+                ", fullName=" + fullName +
+                '}';
+    }
+
 }
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author firstAuthor = new Author("Archibald", "Cronin");
+        Author secondAuthor = new Author("John","Priestley");
+        BooK firstBooK = new BooK("Green Years",1931,firstAuthor);
+        BooK secondBooK = new BooK("A bend in the Road", 1932, secondAuthor);
+
+        System.out.println();
+        System.out.println("Book One");
+        System.out.println(" "+firstBooK.getTitleOfTheBook()+","+
+                " "+ firstBooK.getYaerOfPublicationOfTheBook()+","+
+                " " + firstBooK.getFullName());
+        System.out.println();
+        System.out.println("Book Two");
+        System.out.println(" "+ secondBooK.getTitleOfTheBook()+","+
+                " "+ secondBooK.getYaerOfPublicationOfTheBook()+","+
+                " " + secondBooK.getFullName());
     }
 }
